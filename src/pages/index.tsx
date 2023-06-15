47c49ebcd40ff0957474587ce08259c66aeaf4ba
root <root@DESKTOP-8RLE308>
2023-06-15 11:20:18 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import React, {useState,useEffect} from "react";
+import db from "./firebase";
 
 export default function Home() {
+  const [data, setData] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const post = async () => {
+    setData("名前: " + name + " / メッセージ: " + message);
+    setName("");
+    setMessage("");
+  };
+
   return (
     <>
       <Head>
@@ -11,7 +23,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <p>Hello, world!</p>
+      <div>
+        <p>
+	        名前:
+	        <br />
+	        <input
+	          type="text"
+	          value={name}
+	          onChange={(e) => setName(e.target.value)}
+	        />
+        </p>
+        <p>
+	        内容:
+	        <br />
+	        <textarea
+	          value={message}
+	          onChange={(e) => setMessage(e.target.value)}
+	        ></textarea>
+        </p>
+        <p>
+	        <button onClick={post}>投稿</button>
+        </p>
+      </div>
+      <hr />
+      <div>{data}</div>
       </main>
     </>
   );
